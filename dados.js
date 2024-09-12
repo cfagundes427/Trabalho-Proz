a0d11e654b17716e3eb7542be6ec83cc3616e13c
cfagundes427 <168384188+cfagundes427@users.noreply.github.com>
2024-09-11 22:49:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo: "Frank Ocean",
+        descricao: "Frank Ocean é um cantor e compositor americano que revolucionou o R&B com suas letras profundas e estilo único. Conhecido por sua abordagem inovadora e introspectiva, Frank é uma das figuras mais influentes da música contemporânea.",
+        link: "https://pt.wikipedia.org/wiki/Frank_Ocean",
+        tags: "r&b cantor "
+    },
+    {
+        titulo: "Tyler, The Creator",
+        descricao: "Tyler, The Creator é um rapper, produtor e designer americano, conhecido por sua criatividade irreverente e letras provocativas, misturando hip-hop com elementos experimentais em álbuns como Igor e Flower Boy, que exploram temas de identidade, amor e autoexpressão.",
+        link: "https://pt.wikipedia.org/wiki/Tyler,_the_Creator",
+        tags: "cantor rap rapper "
+    },
+    {
+        titulo: "Lana del Rey",
+        descricao: "Lana Del Rey é uma cantora e compositora americana, famosa por seu estilo nostálgico e cinematográfico, com músicas que exploram temas de melancolia, romance e glamour, como em seus álbuns icônicos Born to Die e Norman Fucking Rockwell.",
+        link: "https://pt.wikipedia.org/wiki/Lana_Del_Rey",
+        tags: "cantora cherry"
+    },
+    {
+        titulo: "Shawn Mendes",
+        descricao: "Shawn Mendes é um cantor e compositor canadense que ganhou fama com seus vídeos no Vine e se destacou com hits como Stitches e álbuns de sucesso como Handwritten e Wonder. Conhecido por suas baladas pop e performances ao vivo, ele é uma figura importante na música contemporânea.",
+        link: "https://pt.wikipedia.org/wiki/Shawn_Mendes",
+        tags: "cantor canadense"
+    },
+    {
+        titulo: "Blackpink",
+        descricao: "BLACKPINK é um grupo de K-pop sul-coreano, formado em 2016, composto por Jisoo, Jennie, Rosé e Lisa. Famoso por suas performances vibrantes e sucessos como Ddu-Du Ddu-Du é um dos grupos mais populares e influentes do cenário musical mundial.",
+        link: "https://pt.wikipedia.org/wiki/Black_Pink",
+        tags: "banda kpop"
+    },
+    {
+        titulo: "Dua Lipa",
+        descricao: "Dua Lipa é uma cantora britânica que se destacou com o álbum Dua Lipa (2017) e ganhou ainda mais fama com Future Nostalgia (2020). Ela é conhecida por sucessos como New Rules e Don't Start Now.",
+        link: "https://pt.wikipedia.org/wiki/Dua_Lipa",
+        tags: "cantora britanica"
+    },
+
+];
